f4dbe5a9aba7aada17ff2b06cb5250b1037fe7c0
Nguyễn Huyền Phương <stu735105086@hnue.edu.vn>
2025-11-20 15:08:24 +0700

    theme

--- app/src/main/java/com/example/study_app/MainActivity.java
+++ app/src/main/java/com/example/study_app/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.study_app.ui.Deadline.MainDeadLine;
@@ -16,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     Button btnDeadLine, btnSubject;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         btnDeadLine=findViewById(R.id.btnDeadLine);
